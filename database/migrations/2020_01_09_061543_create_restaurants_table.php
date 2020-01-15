3a75e437f0036bef9ac79da424064f072ba35a7e
lulu1417 <boa1417@gmail.com>
2020-01-15 22:40:23 +0800

    feature:model

---
+++ database/migrations/2020_01_09_061543_create_restaurants_table.php
@@ -0,0 +1,40 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateRestaurantsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('restaurants', function (Blueprint $table) {
+            $table->bigIncrements('id');
+            $table->string('name');
+            $table->string('class');
+            $table->string('logo')->nullable();
+            $table->string('image')->nullable();
+            $table->string('coordinate');
+            $table->time('operating_time');
+            $table->string('address');
+            $table->string('link');
+            $table->integer('assessment');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('restaurants');
+    }
+}
