2cd3bd07ce81a4b0d76755db92358763b99d55da
lulu1417 <boa@gmail.com>
2020-02-20 21:55:42 +0800

    debug:class empty

--- app/Http/Controllers/RestaurantController.php
+++ app/Http/Controllers/RestaurantController.php
@@ -156,17 +156,15 @@ class RestaurantController extends Controller
         $filted = Restaurant::with(array('foods' =>
 
             function ($query) use ($conditions) {
-                $minimal = $conditions['only_remaining'] ? 1 : 0;
+                $minimal = $conditions['only_remaining'] ? 19 : 0;
                 $query->where('remaining', '>=', $minimal);
             }))
-            //Todo, should check value is not null
-            ->when(!isset($conditions['class']), function ($query) use ($conditions) {
+            ->when(!empty($conditions['class']), function ($query) use ($conditions) {
                 $classes = $conditions['class'];
                 $query->whereIn('class', $classes);
             })
             ->get()
             ->toArray();
-
         $filted = array_filter($filted, function ($restaurant) use($conditions) {
             $calculateDistance = new calculateDistance();
             $restaurant['distance'] =
