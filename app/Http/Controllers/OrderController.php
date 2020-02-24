12df11580dad4817e35c3aa09e784b9d25e32974
lulu1417 <boa@gmail.com>
2020-02-24 13:54:22 +0800

    debug:order complete&cancel

--- app/Http/Controllers/OrderController.php
+++ app/Http/Controllers/OrderController.php
@@ -9,15 +9,16 @@ use Illuminate\Support\Facades\DB;
 
 class OrderController extends Controller
 {
-    function store(Request $request){
+    function store(Request $request)
+    {
 
-        try{
+        try {
             DB::beginTransaction();
             $food = Food::find($request->food_id);
             $request->validate([
                 'user_id' => ['required', 'exists:users,id'],
                 'food_id' => ['required', 'exists:foods,id'],
-                'food_number' => ['required','lte:'.$food->remaining],
+                'food_number' => ['required', 'lte:' . $food->remaining],
             ]);
 
             $create = Order::create([
@@ -34,51 +35,69 @@ class OrderController extends Controller
 
             DB::commit();
             return response()->json($create);
-        }catch (Exception $e){
+        } catch (Exception $e) {
             DB::rollBack();
             return response()->json($e, 400);
         }
 
     }
 
-    function complete($id){
-        if($order = Order::find($id)){
+    function complete($id)
+    {
 
-        }else{
-            return response()->json('order not found',400);
+        if (count(Order::where('id', $id)->get()) > 0) {
+            $order = Order::find($id);
+        } else {
+            return response()->json('order not found', 400);
         }
-        $order->update([
-           'complete' => true,
-        ]);
-        return response()->json($order);
-    }
-
-    function cancel($id){
-
-        if($order = Order::find($id)){
-
-        }else{
-            return response()->json('order not found',400);
-        }
-        if($order->send){
+        if ($order->send) {
             $order->update([
-                'send' => false,
+                'complete' => 1,
             ]);
-            $food = Food::find($order->food_id);
-            $food->update([
-                'remaining' => $food->remaining + $order->food_number,
-            ]);
+            return response()->json($order);
+        } else {
+            return response()->json('the order has been canceled', 400);
         }
 
-        return response()->json($order);
     }
 
-    function look($id){
-        $order = Order::with('food', 'user')->find($id);
-        return response()->json($order);
+    function cancel($id)
+    {
+
+        if (count(Order::where('id', $id)->get()) > 0) {
+            $order = Order::find($id);
+            if ($order->send) {
+                $order->update([
+                    'send' => 0,
+                ]);
+                $food = Food::find($order->food_id);
+                $food->update([
+                    'remaining' => $food->remaining + $order->food_number,
+                ]);
+            } else {
+                return response()->json('the order has been canceled', 400);
+            }
+
+            return response()->json($order);
+        } else {
+            return response()->json('the given order id not found', 400);
+        }
+
     }
 
-    function index(){
+    function look($id)
+    {
+        if (count(Order::where('id', $id)->get()) > 0) {
+            $order = Order::with('food', 'user')->find($id);
+            return response()->json($order);
+        } else {
+            return response()->json('the given order not found', 400);
+        }
+
+    }
+
+    function index()
+    {
         return response()->json(Order::all());
     }
 }
