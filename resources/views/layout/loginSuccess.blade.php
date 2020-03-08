80d5e45482d631bb236b79b12e618ec4637c2e3a
lulu1417 <boa1417@gmail.com>
2020-03-08 11:50:40 +0800

    test:parameter all user data in login succes blade

--- resources/views/layout/loginSuccess.blade.php
+++ resources/views/layout/loginSuccess.blade.php
@@ -7,9 +7,18 @@
 <body>
 <script>
     var message = new Object();
-    console.log('{{$user['name']}}');
+
     message.status = 'success';
-    message.content = '{{$user}}'
+    message.content.id = '{{$user['id']}}'
+    message.content.name = '{{$user['name']}}'
+    message.content.account = '{{$user['account']}}'
+    message.content.api_token = '{{$user['api_token']}}'
+    message.content.image = '{{$user['image']}}'
+    message.content.email = '{{$user['email']}}'
+    message.content.point = '{{$user['point']}}'
+    message.content.phone = '{{$user['phone']}}'
+
+    console.log(message)
 
     window.opener.postMessage(message, '*');
     window.close();
